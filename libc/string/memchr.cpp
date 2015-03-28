bffc84c94724c7018481533f6226cb0cdb2f35ff
Morten Kristensen <morten@burator.com>
2015-03-29 00:00:27 +0100

    Fixed pointer arithmetic warning in memchr.

--- libc/string/memchr.cpp
+++ libc/string/memchr.cpp
@@ -2,9 +2,10 @@
 #include <stdint.h>
 
 const void *memchr(const void *ptr, int value, size_t num) {
+  auto *ptr_ = (const uint8_t*) ptr;
   for (size_t i = 0; i < num; i++) {
-    if (((uint8_t*) ptr)[i] == (uint8_t) value) {
-      return ptr+i;
+    if (ptr_[i] == (uint8_t) value) {
+      return ptr_+i;
     }
   }
   return NULL;
