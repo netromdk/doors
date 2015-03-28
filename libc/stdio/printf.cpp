cba81129f98c12a501ae231b539445b331b116f9
Morten Kristensen <msk@nullpointer.dk>
2015-03-28 16:14:44 +0100

    printf() returns correct chars written.

--- libc/stdio/printf.cpp
+++ libc/stdio/printf.cpp
@@ -5,11 +5,13 @@ int printf(const char *format, ...) {
   char **args = (char**) &format;
   args++;
 
+  int written = 0;
   char buf[64];
   char c;
   while ((c = *format++)) {
     if (c != '%') {
       putchar(c);
+      written++;
       continue;
     }
 
@@ -22,40 +24,40 @@ int printf(const char *format, ...) {
       if (!tmp) {
         tmp = (char*) "(NULL)";
       }
-      puts(tmp);
+      written += puts(tmp);
       break;
 
     case 'c': // character
       putchar((char) *((int32_t*) args++));
+      written++;
       break;
 
     case 'b': // binary
       itos(*((int32_t*) args++), buf, 2);
-      puts(buf);
+      written += puts(buf);
       break;
 
     case 'o': // octal
       itos(*((int32_t*) args++), buf, 8);
-      puts(buf);
+      written += puts(buf);
       break;
 
     case 'd': // decimal
       itos(*((int32_t*) args++), buf, 10);
-      puts(buf);
+      written += puts(buf);
       break;
 
     case 'x': // hexadecimal
       itos(*((int32_t*) args++), buf, 16);
-      puts(buf);
+      written += puts(buf);
       break;
 
     case 'u': // unsigned integer
       utos(*((uint32_t*) args++), buf);
-      puts(buf);
+      written += puts(buf);
       break;
     }
   }
 
-  // TODO: return bytes written!
-  return 0;
+  return written;
 }
