36c9e434992293b7a409de0613f76ef3acf3f49e
Morten Kristensen <morten@burator.com>
2015-03-29 01:17:56 +0100

    Added extern 'C' when cpp for libc string.h and stdlib.h

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -4,7 +4,15 @@
 #include <sys/cdefs.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void itos(int32_t num, char *str, uint8_t base = 10);
 void utos(uint32_t num, char *str, uint8_t base = 10);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // STDLIB_H
