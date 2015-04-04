385096e2cd5772750804d077f998d53cdc3578c4
Morten Kristensen <msk@nullpointer.dk>
2015-04-04 18:33:51 +0200

    Implemented variadic template version of printf() + updated tests.

--- libc++/include/stdio.h
+++ libc++/include/stdio.h
@@ -2,9 +2,139 @@
 #define STDIO_H
 
 #include <sys/cdefs.h>
+#include <stdlib.h>
 
 int putchar(int ic);
 int puts(const char *str);
-int printf(const char *format, ...);
+
+/** printf **/
+
+namespace {
+  int fmtToBase(char fmt) {
+    switch (fmt) {
+    default:
+    case 'd': // decimal
+      return 10;
+
+    case 'b': // binary
+      return 2;
+
+    case 'o': // octal
+      return 8;
+
+    case 'x': // hexadecimal
+      return 16;
+    }
+  }
+
+  bool fmtIsUnsigned(char fmt) {
+    return fmt == 'u';
+  }
+
+  bool fmtIsChar(char fmt) {
+    return fmt == 'c';
+  }
+
+  template <typename T>
+  inline int _printf(T /*value*/, char /*fmt*/) {
+    // Implement specialization!
+    // TODO: Use assert here!
+    return 0;
+  }
+
+  template <>
+  inline int _printf(const char *value, char /*fmt*/) {
+    if (!value) {
+      value = (char*) "(NULL)";
+    }
+    return puts(value);
+  }
+
+  template <>
+  inline int _printf(char *value, char fmt) {
+    return _printf((const char*) value, fmt);
+  }
+
+  template <>
+  inline int _printf(uint32_t value, char fmt) {
+    char buf[255];
+    utos(value, buf, fmtToBase(fmt));
+    return puts(buf);
+  }
+
+  template <>
+  inline int _printf(int/*32_t*/ value, char fmt) {
+    char buf[255];
+    if (fmtIsUnsigned(fmt)) {
+      return _printf((uint32_t) value, fmt);
+    }
+    itos(value, buf, fmtToBase(fmt));
+    return puts(buf);
+  }
+
+  template <>
+  inline int _printf(unsigned char value, char fmt) {
+    if (fmtIsChar(fmt)) {
+      return putchar(value);
+    }
+    return _printf((uint32_t) value, fmt);
+  }
+
+  template <>
+  inline int _printf(char value, char fmt) {
+    if (fmtIsUnsigned(fmt)) {
+      return _printf((unsigned char) value, fmt);
+    }
+    else if (fmtIsChar(fmt)) {
+      return putchar(value);
+    }
+    return _printf((int) value, fmt);
+  }
+
+  template <>
+  inline int _printf(uint64_t value, char fmt) {
+    char buf[255];
+    ltos(value, buf, fmtToBase(fmt));
+    return puts(buf);
+  }
+
+  template <>
+  inline int _printf(int64_t value, char fmt) {
+    char buf[255];
+    if (fmtIsUnsigned(fmt)) {
+      return _printf((uint64_t) value, fmt);
+    }
+    ltos(value, buf, fmtToBase(fmt));
+    return puts(buf);
+  }
+}
+
+// Base case when there are no more arguments.
+inline int printf(const char *format) {
+  return puts(format);
+}
+
+template <typename T, typename... Args>
+inline int printf(const char *format, T value, Args... args) {
+  int written = 0;
+  char c;
+  while ((c = *format++)) {
+    if (c == '%') {
+      char fmt = *format;
+
+      // Print the current value.
+      written += _printf(value, fmt);
+
+      // Continue with the rest of the arguments.
+      written += printf(format+1, args...);
+      break;
+    }
+    else {
+      putchar(c);
+      written++;
+    }
+  }
+  return written;
+}
 
 #endif // STDIO_H
--- tests/libc++/stdio/printf.cpp
+++ tests/libc++/stdio/printf.cpp
@@ -1,21 +1,9 @@
 #include <stdio.h>
 
-#include <string.h> //remove
-
-// Revisit this test when printf() has been rewritten to using
-// variadic templates instead.
-
 int main() {
   if (printf("hello world\n") != 12) {
     return 1;
   }
-
-  /*
-  // yields 7 right now but must be 6!
-  // for some reason the strlen(tmp) in printf() yields 6 instead of 5..
-  return printf("%s\n", "hello");
-  // TODO:
-  // Try implementing printf() differently with va_args?
   
   // hello = +5
   // \n = +1
@@ -32,6 +20,28 @@ int main() {
   if (printf("hello %s\n", "world") != 12) {
     return 3;
   }
-  */
+
+  // I = +1
+  // space = +1
+  // am = +2
+  // space = +1
+  // 1337 = 0x539 = +3
+  // \n = +1
+  // = 9
+  if (printf("I am %x\n", 1337) != 9) {
+    return 4;
+  }
+
+  // Unsign = +6
+  // space = +1
+  // this = +4
+  // space = +1
+  // -1 = (unsigned) 18446744073709551615 = +21
+  // = 33
+  int64_t n = -1;
+  if (printf("Unsign this %u\n", n) != 33) {
+    return 5;
+  }
+
   return 0;
 }
