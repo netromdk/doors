afd9291f4b1c5102372891e6ce0f4523717f0d1c
Morten Kristensen <msk@nullpointer.dk>
2015-04-04 13:46:47 +0200

    Added ltos() test.

--- tests/libc/stdlib/ltos.cpp
+++ tests/libc/stdlib/ltos.cpp
@@ -4,30 +4,30 @@
 int main() {
   // Decimal.
   char str[64] = {0};
-  uint32_t num = 1000000;
+  uint64_t num = 100000000000;
   ltos(num, str);
-  if (strcmp(str, "1000000") != 0) {
+  if (strcmp(str, "100000000000") != 0) {
     return 1;
   }
 
   // Hexadecimal.
   memset(str, 0, 64);
   ltos(num, str, 16);
-  if (strcmp(str, "f4240") != 0) {
+  if (strcmp(str, "174876e800") != 0) {
     return 2;
   }
 
   // Octal.
   memset(str, 0, 64);
   ltos(num, str, 8);
-  if (strcmp(str, "3641100") != 0) {
+  if (strcmp(str, "1351035564000") != 0) {
     return 3;
   }
 
   // Binary.
   memset(str, 0, 64);
   ltos(num, str, 2);
-  if (strcmp(str, "11110100001001000000") != 0) {
+  if (strcmp(str, "1011101001000011101101110100000000000") != 0) {
     return 4;
   }
 
