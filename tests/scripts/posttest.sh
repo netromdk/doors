f5ebe84f5236d4f2346dfa82886282f49b680ec4
Morten Kristensen <msk@nullpointer.dk>
2015-04-04 12:27:56 +0200

    Show test summary.

--- tests/scripts/posttest.sh
+++ tests/scripts/posttest.sh
@@ -3,9 +3,12 @@
 
 YES=$(grep -i "yes" ${CNTFILE} | wc -l | awk '{print $1}')
 NO=$(grep -i "no" ${CNTFILE} | wc -l | awk '{print $1}')
+NUM=$(($YES+$NO))
 
-echo "Tests succeeded: $YES"
-echo "Tests failed:    $NO"
+echo "\n=== Summary ==="
+echo "Tests:     $NUM"
+echo "Succeeded: $YES"
+echo "Failed:    $NO"
 
 if [ $NO -eq 0 ]; then
     echo "ALL GOOD!"
