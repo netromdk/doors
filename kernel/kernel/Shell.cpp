e868715d2d989653056908156f353c46eec5ac80
Morten Kristensen <me@mortens.dev>
2026-06-19 09:15:53 +0200

    Implement simple kernel shell

---
+++ kernel/kernel/Shell.cpp
@@ -0,0 +1,77 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <kernel/Kbd.h>
+#include <kernel/Shell.h>
+
+namespace {
+
+constexpr int MAX_CMDS = 32;
+Command cmdTable[MAX_CMDS];
+int numCmds = 0;
+
+} // namespace
+
+void Shell::run()
+{
+  char line[256];
+  char *argv[16];
+  constexpr int MAX_ARGS = sizeof(argv) / sizeof(*argv);
+  for (;;) {
+    printf("> ");
+    Kbd::readLine(line, sizeof(line));
+    int argc = Shell::tokenize(line, argv, MAX_ARGS);
+    Shell::dispatch(argc, argv);
+  }
+}
+
+int Shell::tokenize(char *line, char **argv, int max)
+{
+  int argc = 0;
+  char *p = line;
+  while (*p) {
+    while (isspace(*p)) {
+      p++;
+    }
+    if (!*p) {
+      break;
+    }
+    if (argc >= max - 1) {
+      break;
+    }
+    argv[argc++] = p;
+    while (*p && !isspace(*p)) {
+      p++;
+    }
+    if (*p) {
+      *p++ = '\0';
+    }
+  }
+  argv[argc] = nullptr;
+  return argc;
+}
+
+bool Shell::dispatch(int argc, char **argv)
+{
+  if (argc == 0) {
+    return true;
+  }
+
+  for (int i = 0; i < numCmds; i++) {
+    if (strcmp(argv[0], cmdTable[i].name) == 0) {
+      cmdTable[i].handler(argc, argv);
+      return true;
+    }
+  }
+
+  printf("Unknown command: %s\n", argv[0]);
+  return false;
+}
+
+void Shell::registerCmd(const Command &cmd)
+{
+  if (numCmds < MAX_CMDS) {
+    cmdTable[numCmds++] = cmd;
+  }
+}
