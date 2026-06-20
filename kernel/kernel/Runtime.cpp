9ab6e2d8dca209a8e6350c0637ca59d2ce6409f5
Morten Kristensen <me@mortens.dev>
2026-06-20 16:53:23 +0200

    Once string is used in static arrays __cxa_atexit must be defined
    However, since the kernel never exits the destructors (via `__cxa_atexit`) are never called and thus
    it is just a stub.

--- kernel/kernel/Runtime.cpp
+++ kernel/kernel/Runtime.cpp
@@ -9,3 +9,9 @@ void __cxa_guard_release(__guard *g)
 {
   *g = 1;
 }
+
+extern "C" int __cxa_atexit(void (*)(void *), void *, void *)
+{
+  // Kernel never exits, so destructors registered here are never called.
+  return 0;
+}
