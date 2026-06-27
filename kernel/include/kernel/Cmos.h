9fc8bce02bc4f482b26ba5289c81fbd539e13db7
Morten Kristensen <me@mortens.dev>
2026-06-27 09:52:09 +0200

    CMOS clock accessor

--- kernel/include/kernel/Cmos.h
+++ kernel/include/kernel/Cmos.h
@@ -6,6 +6,7 @@
 class Cmos {
 public:
   static void printTime();
+  static void readTime(uint8_t &hours, uint8_t &minutes, uint8_t &seconds);
 
   /**
    * Seconds since Unix Epoch (January 1, 1970 00:00:00) not counting
