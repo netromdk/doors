046cef3407141f0aefbcd80246fcaaf8b7f3b69b
Morten Kristensen <me@mortens.dev>
2026-06-28 11:13:41 +0200

    Implement all_of/any_of/none_of in <algorithm>

---
+++ tests/libc++/algorithm/none_of.cc
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <doctest/doctest.h>
+
+TEST_CASE("none_of true")
+{
+  int arr[] = {1, 3, 5, 7};
+  CHECK(none_of(arr, arr + 4, [](int x) { return x % 2 == 0; }));
+}
+
+TEST_CASE("none_of false")
+{
+  int arr[] = {1, 3, 5, 6};
+  CHECK(!none_of(arr, arr + 4, [](int x) { return x % 2 == 0; }));
+}
+
+TEST_CASE("none_of empty")
+{
+  int arr[] = {1, 2, 3};
+  CHECK(none_of(arr, arr, [](int x) { return true; }));
+}
