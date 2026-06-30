bac49d3f0b84c509c06c5d180815190049be928f
Morten Kristensen <me@mortens.dev>
2026-06-30 05:52:32 +0200

    Move non-templates to .cc to not have them in each TU using <cstdio>

---
+++ libc++/stdio/format.cc
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+namespace detail {
+
+void formatRaw(SnprintfBuf &buf, const char *value, char)
+{
+  buf.writestr(value);
+}
+
+void formatRaw(SnprintfBuf &buf, char *value, char fmt)
+{
+  formatRaw(buf, (const char *) value, fmt);
+}
+
+void formatRaw(SnprintfBuf &buf, unsigned char value, char fmt)
+{
+  if (fmtIsChar(fmt)) {
+    buf.put(value);
+    return;
+  }
+  formatRaw(buf, static_cast<uint32_t>(value), fmt);
+}
+
+void formatRaw(SnprintfBuf &buf, char value, char fmt)
+{
+  if (fmtIsUnsigned(fmt)) {
+    formatRaw(buf, static_cast<unsigned char>(value), fmt);
+  }
+  else if (fmtIsChar(fmt)) {
+    buf.put(value);
+  }
+  else {
+    formatRaw(buf, static_cast<int>(value), fmt);
+  }
+}
+
+void formatRaw(SnprintfBuf &buf, bool value, char fmt)
+{
+  if (fmtIsBool(fmt)) {
+    buf.writestr(value ? "true" : "false");
+  }
+  else {
+    buf.writestr(value ? "1" : "0");
+  }
+}
+
+void formatRaw(SnprintfBuf &buf, const void *value, char)
+{
+  buf.put('0');
+  buf.put('x');
+  const unsigned long addr = reinterpret_cast<unsigned long>(value);
+  char tmp[65];
+  ltos(addr, tmp, 16, false);
+  for (char *p = tmp; *p; p++) {
+    buf.put(*p);
+  }
+}
+
+void formatRaw(SnprintfBuf &buf, const int *value, char fmt)
+{
+  formatRaw(buf, (const void *) value, fmt);
+}
+
+void formatRaw(SnprintfBuf &buf, int *value, char fmt)
+{
+  formatRaw(buf, (const void *) value, fmt);
+}
+
+void formatRaw(SnprintfBuf &buf, decltype(nullptr), char fmt)
+{
+  formatRaw(buf, (const void *) nullptr, fmt);
+}
+
+} // namespace detail
+
+int printf(const char *format)
+{
+  char buf[2048];
+  const int n = snprintf(buf, sizeof(buf), format);
+  puts(buf);
+  return n;
+}
