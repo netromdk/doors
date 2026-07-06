a2bf3298c6f581c711f6d08befbb7efaf41a416b
Morten Kristensen <me@mortens.dev>
2026-07-06 15:39:46 +0200

    Fix strncmp() ignoring length differential

--- libc++/string/strncmp.cc
+++ libc++/string/strncmp.cc
@@ -1,9 +1,14 @@
-#include <algorithm>
 #include <cstring>
 
 int strncmp(const char *str1, const char *str2, size_t num)
 {
-  size_t len1 = strlen(str1), len2 = strlen(str2);
-  size_t m = min(len1, len2);
-  return memcmp(str1, str2, min(m, num));
+  for (size_t i = 0; i < num; ++i) {
+    if (str1[i] != str2[i]) {
+      return static_cast<unsigned char>(str1[i]) - static_cast<unsigned char>(str2[i]);
+    }
+    if (str1[i] == '\0') {
+      return 0;
+    }
+  }
+  return 0;
 }
--- tests/libc++/string/strncmp.cc
+++ tests/libc++/string/strncmp.cc
@@ -8,6 +8,23 @@ TEST_CASE("strncmp")
   // Compares using the minimum length of inputs and 200 = 5.
   CHECK(strncmp("hello", "hello", 200) == 0);
 
-  CHECK(strncmp("1", "a", 1) == -1);
-  CHECK(strncmp("a", "1", 1) == 1);
+  CHECK(strncmp("1", "a", 1) < 0);
+  CHECK(strncmp("a", "1", 1) > 0);
+
+  // Length differential: shorter string is a prefix.
+  CHECK(strncmp("abc", "abcdef", 6) < 0);
+  CHECK(strncmp("abcdef", "abc", 6) > 0);
+
+  // Limited to num.
+  CHECK(strncmp("abc", "abcdef", 3) == 0);
+  CHECK(strncmp("abcdef", "abc", 3) == 0);
+
+  // num=0 always returns 0.
+  CHECK(strncmp("anything", "anything", 0) == 0);
+  CHECK(strncmp("abc", "xyz", 0) == 0);
+
+  // Empty string vs. non-empty.
+  CHECK(strncmp("", "a", 1) < 0);
+  CHECK(strncmp("a", "", 1) > 0);
+  CHECK(strncmp("", "", 1) == 0);
 }
